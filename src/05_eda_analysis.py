a9000fc37e25962c7a1b1a9cefbbf6243a8a33cc
Nouhaila EL YEFRAOUI <elyefraouinouhaila@gmail.com>
2026-03-24 11:06:59 +0100

    update 05_eda_analysis.py

--- src/05_eda_analysis.py
+++ src/05_eda_analysis.py
@@ -15,7 +15,7 @@ df_recettes_secteur = pd.read_sql("SELECT * FROM recettes_secteur", engine)
 df_recettes_antenne = pd.read_sql("SELECT * FROM recettes_antenne", engine)
 df_recettes_adherent = pd.read_sql("SELECT * FROM recettes_adherent", engine)
 
-# 3. Evolution mensuelle des recettes
+# 2. Evolution mensuelle des recettes
 plt.figure(figsize=(10, 5))
 plt.plot(df_recettes_mensuelles["date"], df_recettes_mensuelles["recettes"], marker="o")
 plt.title("Évolution mensuelle des recettes")
@@ -25,7 +25,7 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-# 4. Evolution annuelle des recettes
+# 3. Evolution annuelle des recettes
 recettes_annuelles = df_recettes_mensuelles.groupby("annee", as_index=False).agg(
     recettes_totales=("recettes", "sum"),
     transactions_totales=("transactions", "sum")
@@ -46,7 +46,7 @@ plt.ylabel("Recettes totales")
 plt.tight_layout()
 plt.show()
 
-# 5. Saisonnalité mensuelle
+# 4. Saisonnalité mensuelle
 saisonnalite = df_recettes_mensuelles.groupby("month", as_index=False).agg(
     recettes_totales=("recettes", "sum"),
     transactions_totales=("transactions", "sum")
@@ -68,7 +68,7 @@ plt.ylabel("Recettes totales")
 plt.tight_layout()
 plt.show()
 
-# 6. Analyse par produit
+# 5. Analyse par produit
 analyse_produit = df_recettes_produit.groupby(["produit", "annee"], as_index=False).agg(
     recettes_totales=("recettes", "sum")
 )
@@ -96,16 +96,14 @@ plt.legend(title="Année")
 plt.tight_layout()
 plt.show()
 
-# 7. Analyse par secteur
+# 6. Analyse par secteur
 analyse_secteur = df_recettes_secteur.groupby(["secteur", "annee"], as_index=False).agg(
     recettes_totales=("recettes", "sum")
 )
-
 analyse_secteur["part_%"] = (
     analyse_secteur["recettes_totales"] /
     analyse_secteur["recettes_totales"].sum()
 ) * 100
-
 analyse_secteur = analyse_secteur.sort_values("recettes_totales", ascending=False)
 
 print("\nAnalyse par secteur")
@@ -122,26 +120,21 @@ sns.barplot(
     y="recettes_totales",
     hue="annee"
 )
-
 plt.title("Recettes par secteur et par année")
 plt.xticks(rotation=45)
 plt.legend(title="Année")
 plt.tight_layout()
-
 plt.show()
 
 
-# 8. Analyse par antenne
+# . Analyse par antenne
 analyse_antenne = df_recettes_antenne.groupby(["antenne", "annee"], as_index=False).agg(
     recettes_totales=("recettes", "sum")
 )
-
-
 analyse_antenne["part_%"] = (
     analyse_antenne["recettes_totales"] /
     analyse_antenne["recettes_totales"].sum()
 ) * 100
-
 analyse_antenne = analyse_antenne.sort_values("recettes_totales", ascending=False)
 
 print("\nAnalyse par antenne")
@@ -160,12 +153,11 @@ plt.legend(title="Année")
 plt.tight_layout()
 plt.show()
 
-# 9. Analyse par type d’adhérent
+# 8. Analyse par type d’adhérent
 analyse_adherent = df_recettes_adherent.groupby("personne_physique", as_index=False).agg(
     recettes_totales=("recettes", "sum"),
     transactions_totales=("transactions", "sum")
 )
-
 analyse_adherent["part_%"] = (
     analyse_adherent["recettes_totales"] /
     analyse_adherent["recettes_totales"].sum()
@@ -183,11 +175,6 @@ plt.pie(
 plt.title("Part des recettes par type d’adhérent")
 plt.tight_layout()
 plt.show()
-
-
-
-
-
 
 
 print("\nEDA terminé avec succès.")
